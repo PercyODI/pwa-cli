8e5ca0985ab380173d1f273c8ff08368b39a8683
Pearse Hutson <pearse.2008@gmail.com>
2018-12-18 15:33:48 -0600

    added counter components.

---
+++ src/components/CounterComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="counter">
+    <CounterChanger />
+    <CounterViewer />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import CounterChanger from '@/components/CounterChanger.vue'; // @ is an alias to /src
+import CounterViewer from '@/components/CounterViewer.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    CounterChanger, CounterViewer,
+  },
+})
+export default class CounterComponent extends Vue {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Counter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <CounterComponent/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import CounterComponent from '@/components/CounterComponent.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    CounterComponent,
+  },
+})
+export default class Counter extends Vue {}
+</script>
